fcbb0235f74610c062843b52108651b8f0d939b4
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2023-01-06 14:20:15 +0300

    1.0

---
+++ CoreDataApp/CoreDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Евгений Забродский on 6.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
